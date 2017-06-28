9e4329e014ad8cecbe56c3511e174edeee0c8c84
lianboi <lbthomte@yahoo.com>
2017-06-28 15:56:29 +0530

    added onAuthStateChanged to check user authentication status

--- src/js/auth.js
+++ src/js/auth.js
@@ -8,6 +8,25 @@ function logout() {
   firebase.auth().signOut();
 }
 
+//Checking user authentication status.
+(function checkAuthStatus(){
+  //using onAuthStateChanged
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log('user is signed in,', user);
+      document.getElementById("logout-btn").className = "btn-controls visible";
+      document.getElementById("login-btn").className = "btn-controls hidden";
+    } else {
+      // No user is signed in.
+      console.log("user is not authenticated", user);
+      document.getElementById("logout-btn").className = "btn-controls hidden";
+      document.getElementById("login-btn").className = "btn-controls visible";
+    }
+  });
+})();
+
+
 (function handleRedirect(firebase){
   firebase.auth().getRedirectResult().then(function(result) {
     if (result.credential) {
@@ -20,6 +39,7 @@ function logout() {
         email: 'hello@email.com',
         profile_picture : 'no-link'
       });
+      localStorage.setItem('accessToken', token);
       document.getElementById("logout-btn").className = "btn-controls visible";
       document.getElementById("login-btn").className = "btn-controls hidden";
     }
