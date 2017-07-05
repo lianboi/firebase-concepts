bb99e858204ad9ccdf27deb00c52ec84374aebed
lianboi <lbthomte@yahoo.com>
2017-07-05 15:09:14 +0530

    wip login form

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -2,12 +2,18 @@ function signUp(email, password){
   console.log("arguments", email, password);
   firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
     //user data
+    window.location.href="/";
   }).catch(function(error){
     //error
+    alert("signup error:" + JSON.stringify(error));
     console.log("signup error:", error);
   });
 }
 
 function createAcount(){
-  signUp(document.getElementById('username').value, document.getElementById('password').value);
+  if(document.getElementById('password').value && (document.getElementById('password').value===document.getElementById('confirm-password').value)&& document.getElementById('username').value){
+    signUp(document.getElementById('username').value, document.getElementById('password').value);
+  } else {
+    alert("Please, fill in the signup form properly.");
+  }
 }
